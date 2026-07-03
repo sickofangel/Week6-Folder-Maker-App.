a6ed200a3590ff0493917af8d549fd36d8cd0850
sickofangel <Abautista555@icloud.com>
2026-07-03 12:50:15 -0700

    FileMaker App

---
+++ FileMaker/Module1.vb
@@ -0,0 +1,120 @@
+﻿'TODO:1. Change Procedure name to your own procedure name
+
+'TODO:2.  Add Json package to the resources
+
+'TODO:3. Create A Project Class
+
+'TODO:4.  Create A Json file for the Project Class
+
+'TODO:5.  Refactor writeFile procedure to take a string for data input
+
+'TODO:6.  move the input variable up to the global class variable access
+
+'TODO:7.  Seralize Project Class
+
+'TODO:8.  Deseralize The Project json Class
+
+'TODO:9.  Use snippets (insert comment) to add comments to procedures and functions
+
+'TODO:10.Refactor your code to create subfolders in a separate procedure
+
+'TODO:11.Remove reference comments
+
+Module Module1
+
+    'READ: 'More information on file reading and writing in the coursebook: pg 68: FileRead
+
+    'https://drive.google.com/file/d/1qwb9Sq3bf9sWPdAUeiFX_xM1Knb4Ikpp/view
+
+    Dim ProjectName As String
+
+    Dim FullDirectory As String
+
+    Sub Main()
+
+        Dim input As String = 0
+
+        While input <> "exit"
+
+            Console.WriteLine("please enter product name.")
+
+            ProjectName = Console.ReadLine
+
+            Console.WriteLine("Please enter a command  Exit | create")
+
+            input = Console.ReadLine.ToString()
+
+            If input = "create" Then
+
+                MakeP2PProjectFolders()
+
+            End If
+
+        End While
+
+    End Sub
+
+    Private Sub MakeP2PProjectFolders()
+
+        'TODO: Add Json database
+
+        'TODO: Change MakeP2PProjectFolders to MakeProjectFolders
+
+        Dim newFolderPath As String = My.Computer.FileSystem.SpecialDirectories.Desktop
+
+        If ProjectName = "" Then
+
+            ProjectName = " Not Set\"
+
+        End If
+
+        '  My.Computer.FileSystem.CreateDirectory(newFolderPath + ProjectName)
+
+        CreateProjectFolder(newFolderPath, ProjectName)
+        newFolderPath += "\" + ProjectName
+        FullDirectory = newFolderPath
+
+
+        CreateProjectFolder(newFolderPath, "\Docs")
+        CreateProjectFolder($"{newFolderPath}\Docs", "Refs")
+        CreateProjectFolder($"{newFolderPath}\Docs", "Word")
+        CreateProjectFolder($"{newFolderPath}\Docs", "PDF")
+
+        CreateProjectFolder(newFolderPath, "\Assets")
+        CreateProjectFolder($"{newFolderPath}\Assets", "Art")
+        CreateProjectFolder($"{newFolderPath}\Assets", "Images")
+
+        WriteFile("Readme.txt", newFolderPath)
+        WriteFile("Readme.txt", $"{newFolderPath}\Docs")
+
+
+
+        Console.WriteLine("Project created in: " + FullDirectory)
+
+    End Sub
+
+    Private Sub WriteFile(fileName As String, location As String)
+
+        'Ref:https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/programming/drives-directories-files/how-to-write-text-to-files-with-a-streamwriter
+
+        If fileName <> "" Then
+
+            Dim file As System.IO.StreamWriter
+
+            file = My.Computer.FileSystem.OpenTextFileWriter(location + "\" + fileName + ".txt", True)
+
+            file.WriteLine("This is a readme file for the team")
+
+            file.Close()
+
+        End If
+
+    End Sub
+
+    Sub CreateProjectFolder(newFolderPath As String, ProjectName As String)
+
+        My.Computer.FileSystem.CreateDirectory(newFolderPath + "\" + ProjectName)
+
+    End Sub
+
+End Module
---
+++ FileMaker/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FileMaker")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FileMaker")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("84e88a91-e63f-4651-9f4d-fdc1e69e5ff3")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
